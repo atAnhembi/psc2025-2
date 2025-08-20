1370c26ab6b383ff6bf98ce11f16538a13d6fe81
Emerson Paduan <emerson@paduan.dev.br>
2025-08-19 21:51:47 -0300

    aula 01

---
+++ aula01/PrimeiroPrograma.java
@@ -0,0 +1,30 @@
+
+// sout = System.out.println
+// comentário de linha
+
+/* 
+ * Aula 1 de programação
+ * data: 19/08/2025 
+*/
+
+/*
+  Operadores Aritméticos básicos: + - * /
+
+ */
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+        System.out.println("20 + 5");
+        System.out.println(21 + 5);
+        System.out.println(21 - 5);
+        System.out.println(21 * 5);
+        System.out.println(21 / 5);
+        System.out.println(2 + 3 * 5);
+        System.out.println((2 + 3) * 5);
+        System.out.println("20 + 5 = " + 20 + 5); // concatenar
+        System.out.println("20 + 5 = " + (20 + 5)); // concatenar
+        System.out.println("O resultado é " + (20 + 5)); // concatenar
+    }    
+
+}
---
+++ aula01/SegundoPrograma.java
@@ -0,0 +1,12 @@
+public class SegundoPrograma {
+    public static void main(String[] args) {
+        int numero1; // reserva um espaço na memória para guardar um valor inteiro
+        int numero2; // reserva outro espaço para outro número
+
+        numero1 = 10; // guarda o valçor 10 dentro da variável
+        numero2 = 5;
+        
+        System.out.println(numero1 + numero2);
+
+    }
+}
