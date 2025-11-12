7bd422be06e5c76ff7cef5f80c15b52a0c399bc0
Emerson Paduan <emerson@paduan.dev.br>
2025-11-12 16:53:35 -0300

    aula 19

---
+++ aula19/Main.java
@@ -0,0 +1,12 @@
+
+import model.Estudante;
+
+public class Main {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+
+        estudante.setNome("Fabio");
+
+
+    }
+}
---
+++ aula19/model/Estudante.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Estudante extends Pessoa {
+
+
+    public void teste(){
+        setNome("Fabio");
+    }
+}
